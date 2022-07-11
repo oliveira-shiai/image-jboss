8c6492326bb709932530afa0b51b8296fcde8fb5
oliveira-shiai <47541663+oliveira-shiai@users.noreply.github.com>
2022-07-10 23:42:36 -0300

    Update datasource.sh

--- datasource.sh
+++ datasource.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-sleep 15
+/opt/eap/bin/jboss-cli.sh <<EOT
+embed-server
+module add --name=org.postgres --resources=/opt/eap/postgresql-42.2.5.jar --dependencies=javax.api,javax.transaction.api
+EOT
 
-/opt/eap/bin/jboss-cli.sh -c --command="module add --name=org.postgres --resources=/opt/eap/postgresql-42.2.5.jar --dependencies=javax.api,javax.transaction.api"
 sleep 2
-/opt/eap/bin/jboss-cli.sh -c --command="/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)"
+
+/opt/eap/bin/jboss-cli.sh <<EOT
+embed-server
+/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)
+EOT
+
 sleep 2
-/opt/eap/bin/jboss-cli.sh -c --command="data-source add --jndi-name=java:/PostGreDS --name=PostgrePool --connection-url=jdbc:postgresql://postgresql/appdb --driver-name=postgres --user-name=use --password=pass"
+
+/opt/eap/bin/jboss-cli.sh <<EOT
+embed-server
+data-source add --jndi-name=java:/PostGreDS --name=PostgrePool --connection-url=jdbc:postgresql://postgresql/appdb --driver-name=postgres --user-name=use --password=pass
+EOT
