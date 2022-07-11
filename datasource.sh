ef671eb87bbaca4273fda300163a925698007a3e
oliveira-shiai <47541663+oliveira-shiai@users.noreply.github.com>
2022-07-10 23:00:17 -0300

    Add files via upload

---
+++ datasource.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/opt/eap/bin/jboss-cli.sh <<EOT
+embed-server
+module add --name=org.postgres --resources=/opt/eap/postgresql-42.2.5.jar --dependencies=javax.api,javax.transaction.api
+/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)"
+data-source add --jndi-name=java:/PostGreDS --name=PostgrePool --connection-url=jdbc:postgresql://postgresql/appdb --driver-name=postgres --user-name=use --password=pass
+EOT
