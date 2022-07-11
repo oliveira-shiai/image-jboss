08d4b3ca9d5a2348263daf891470c3a620b4a244
oliveira-shiai <47541663+oliveira-shiai@users.noreply.github.com>
2022-07-10 23:28:40 -0300

    Update datasource.sh

--- datasource.sh
+++ datasource.sh
@@ -3,6 +3,14 @@
 /opt/eap/bin/jboss-cli.sh <<EOT
 embed-server
 module add --name=org.postgres --resources=/opt/eap/postgresql-42.2.5.jar --dependencies=javax.api,javax.transaction.api
+EOT
+
+/opt/eap/bin/jboss-cli.sh <<EOT
+embed-server
 /subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)"
+EOT
+
+/opt/eap/bin/jboss-cli.sh <<EOT
+embed-server
 data-source add --jndi-name=java:/PostGreDS --name=PostgrePool --connection-url=jdbc:postgresql://postgresql/appdb --driver-name=postgres --user-name=use --password=pass
 EOT
