8d94a03f311e5499e1b1be113546f3144b7d9531
oliveira-shiai <47541663+oliveira-shiai@users.noreply.github.com>
2022-07-11 18:26:30 -0300

    Delete datasource.sh

--- datasource.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/opt/eap/bin/jboss-cli.sh <<EOT
-embed-server
-/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)
-EOT
-
-exit
